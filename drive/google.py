f1d7255d162024661bc66d30927dcd15164acb97
William Turner Burkitt <williamturnerburkitt@hotmail.com>
2023-03-25 22:02:06 +0100

    deleted google drive idea

--- drive/google.py
+++
@@ -1,9 +0,0 @@
-# https://stackoverflow.com/questions/67623860/how-to-access-shared-google-drive-files-through-python
-from pydrive2.auth import GoogleAuth
-from oauth2client.service_account import ServiceAccountCredentials
-from pydrive2.drive import GoogleDrive
-
-auth = GoogleAuth()
-auth.LocalWebserverAuth()
-
-drive = GoogleDrive(auth)
